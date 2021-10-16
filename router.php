7447267c005bf171fc980f924e98046b01432e39
Agustina Di Tommaso <agustinaditommaso22@gmail.com>
2021-10-16 09:41:00 -0300

    Se agrega ruta para home y show movies

--- router.php
+++ router.php
@@ -5,3 +5,23 @@ define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"]
 $action = $_GET["action"];
 
 $params = explode('/',$action);
+
+if (!empty($_GET['action'])){
+    $action = $_GET['action'];
+}
+else {
+    $action = 'home';
+}
+
+switch ($params[0]) {
+    case 'home':
+        $this->controller->showHome();
+        break;
+    case 'show':
+        $this->controller->showMoviesByCategorias($params[1]);
+        break;
+    default:
+        echo '404 - Página no encontrada';
+        break;
+        
+}
