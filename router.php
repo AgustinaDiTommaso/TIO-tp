febef487753b6fbeac37fa8818787e19ca368d0c
Agustina Di Tommaso <agustinaditommaso22@gmail.com>
2021-10-16 09:33:22 -0300

    Se agregan comandos para lograr url semanticas

--- router.php
+++ router.php
@@ -1,2 +1,7 @@
 <?php
 
+define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
+
+$action = $_GET["action"];
+
+$params = explode('/',$action);
