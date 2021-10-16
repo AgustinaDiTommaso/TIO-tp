1d686ae292feefdf44b2cbff1ce4a5f8ff006982
Sofia Gutierrez <gsofy18@gmail.com>
2021-10-16 12:33:38 -0300

    se agregan functions en movie controller

--- controller/movie.controller.php
+++ controller/movie.controller.php
@@ -1,1 +1,40 @@
 <?php
+require_once 'model/movie.model.php';
+//require_once 'app/MovieView.php';
+
+class MovieController {
+
+    private $model;
+    private $view;
+
+    public function __construct() {
+        $this->model = new TaskModel();
+        //$this->view = new MovieView();
+    }
+
+    function showMoviesByGenre() {
+
+        // verifica datos obligatorios
+        if (!isset($_GET['genre']) || empty($_GET['genre'])) {
+            $this->view->renderError();
+            return;
+        }
+
+        // obtiene el genero enviado por GET 
+        $genre = $_GET['genre'];
+
+        // obtengo las peliculas del modelo
+        $movies = $this->model->getMoviesByGenre($genre);
+
+        // actualizo la vista
+        $this->view->renderMoviesByGenre($genre, $movies);
+    }
+
+    function showHome() {
+
+
+        // actualizo la vista
+        //$this->view->renderMovies($movies);
+    }
+
+}
