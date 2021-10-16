3aba6497b2950498adce8785180d785c762d0256
Agus <agus.i.s_04@hotmail.com>
2021-10-16 01:40:29 -0300

    creada clase taskModel y aniadida la funcion getMoviesByGenre

---
+++ models/movie.model.php
@@ -0,0 +1,18 @@
+<?php
+
+class TaskModel{
+    private $db;
+    
+    public function __construct(){
+        $this->db = new PDO('mysql:host=localhost;'.'dbname=db_movies;charset=utf8', 'root', '');
+    }
+
+    public function getMoviesByGenre($genre){
+        $query = $this->db ->prepare('SELECT * FROM movies WHERE genre = ?');
+        $query->execute([$genre]);
+        $movies = $query->fetchAll(PDO::FETCH_OBJ);
+
+        return $movies;
+    }
+
+}
